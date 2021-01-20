77e5d63498d79c7a30bfa23d9a8434aa57596109
Abhilash chenna <abhilash.chenna@gmail.com>
2021-01-20 18:51:10 +0530

    redirection updated in Home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -34,10 +34,9 @@ class Home extends CI_Controller {
 		
 		$this->data['state'] = $this->Base_model->get_state_by_id($state_id);
 		$this->data['slides'] = $this->Base_model->get_slides_by_state($state_id);
-
 		$this->template->load('site', 'front_view/home', $this->data);
 	}
-
+/*
 	public function gallery(){
 		$this->data['photos']=$this->crud_model->common_get2('id','files');
 		$this->data['videos']=$this->crud_model->common_get2('id','da_videos_tbl');
@@ -45,20 +44,28 @@ class Home extends CI_Controller {
 		$this->load->view('gallery/gallery_view',$this->data);
 		$this->load->view('footer',$this->data);
 	}
+*/
 	public function gallery_photos()
 	{
+		if(empty($this->session->userdata('state_id')))
+		{
+			redirect(base_url());
+		}
 		// $this->data['photos']=$this->Base_model->get_photos_by_state();
 		$this->data['state'] = $this->Base_model->get_state_by_id($this->session->userdata('state_id'));
 		$this->data['photos']=$this->Base_model->get_all_photos();
-
 		$this->template->load('site', 'front_view/gallery/gallery_photos', $this->data);
 	}
+
 	public function gallery_videos()
 	{
+		if(empty($this->session->userdata('state_id')))
+		{
+			redirect(base_url());
+		}
 		// $this->data['videos']=$this->Base_model->get_videos_by_state();
 		$this->data['state'] = $this->Base_model->get_state_by_id($this->session->userdata('state_id'));
 		$this->data['videos']=$this->Base_model->get_all_videos();
-
 		$this->template->load('site', 'front_view/gallery/gallery_videos', $this->data);
 	}
 }
